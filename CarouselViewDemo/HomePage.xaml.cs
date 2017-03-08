bd0dfa0492181eb0ea20a04b2cad5787d7c3a688
KenChiu <knowledge0210@gmail.com>
2017-03-09 01:13:53 +0800

    init

---
+++ CarouselViewDemo/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarouselViewDemo
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage()
+		{
+			InitializeComponent();
+			BindingContext = new HomeViewModel();
+		}
+
+		public void CarouseItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			var item = e.SelectedItem as DataModel;
+			if (item == null)
+			{
+				return;
+			}
+			lbl.Text = item.Name;
+		}
+	}
+}
---
+++ CarouselViewDemo/HomeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CarouselViewDemo
+{
+	public class HomeViewModel 
+	{
+		public ObservableCollection<DataModel> Datamodels{ get; set; }
+		public string str { get; set; }
+		public HomeViewModel()
+		{
+			Datamodels = new ObservableCollection<DataModel>();
+			Datamodels.Add(new DataModel()
+			{
+				Name = "櫻花1",
+				ImageUrl = "http://pic.pimg.tw/sinea100/1390465910-2545708497.jpg",
+			});
+			Datamodels.Add(new DataModel()
+			{
+				Name = "櫻花2",
+				ImageUrl = "http://tw10005.tw.tranews.com/Show/images/Column/8938_1.jpg",
+			});
+			Datamodels.Add(new DataModel()
+			{
+				Name = "櫻花3",
+				ImageUrl = "http://blossom.tw.tranews.com/Show/images/Column/8936_1.jpg",
+			});
+			Datamodels.Add(new DataModel()
+			{
+				Name = "櫻花4",
+				ImageUrl = "https://farm8.staticflickr.com/7375/16175048917_696be50c12_b.jpg",
+			});
+
+		}
+
+
+
+	}
+
+	public class DataModel
+	{
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
+
